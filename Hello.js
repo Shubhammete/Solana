6e45d12a6e54b0dd8544b51f6b66a69f51d91d34
Shubham Mete <shubhammete9009@gmail.com>
2022-11-25 11:44:23 +0000

    Create 5 files

---
+++ Hello.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+const a = 10;
+const b = 12;
+
+console.log("a",a,"b",b);
+
+if (a>b){
+    console.log("a is greater than b")
+}
+else{
+    console.log("b is greater than a");
+}
+if (a<b){
+    let c = 2;
+    
+    console.log("a is greater than b",c)
+}
+else{
+    console.log("b is greater than a");
+}
---
+++ cowsay.js
@@ -0,0 +1,14 @@
+var cowsay = require("cowsay");
+
+// const person={
+//     name: "Shubham",
+//     surname: "Mete",
+// }
+// console.log(person.name)
+
+console.log(cowsay.say({
+    text : "Shubham is coder",
+    e : "euuu",
+    T : "Oooo",
+    r : 1,
+}));
